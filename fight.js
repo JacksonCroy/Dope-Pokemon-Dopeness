c576600a3e636883baf4af2b3d7ac082e9f2ace2
Ian <ian.shane.sears@gmail.com>
2020-03-19 15:13:41 -0700

    small name changes

--- fight.js
+++ fight.js
@@ -1,6 +1,6 @@
 // constructor function which can take in a series of values and create objects
 // with the properties contained inside
-function Character(name, type, gender, age, strength, hp) {
+function Pokemon(name, type, gender, age, strength, hp) {
   this.name = name;
   this.type = type;
   this.gender = gender;
@@ -9,7 +9,7 @@ function Character(name, type, gender, age, strength, hp) {
   this.hp = hp;
 }
 // method which prints all of the stats for a character
-Character.prototype.printStats = function() {
+Pokemon.prototype.printStats = function() {
   console.log(
     "Name: " +
       this.name +
@@ -29,7 +29,7 @@ Character.prototype.printStats = function() {
 
 // method which determines whether or not a character's "hitpoints" are less than zero
 // and returns true or false depending upon the outcome
-Character.prototype.isAlive = function() {
+Pokemon.prototype.isAlive = function() {
   if (this.hp > 0) {
     console.log(this.name + " is still alive!");
     console.log("\n-------------\n");
@@ -40,8 +40,8 @@ Character.prototype.isAlive = function() {
 };
 
 // method which takes in a second object and decreases their "hitpoints" by this character's strength
-Character.prototype.attack = function(character2) {
-   var idVar = setInterval(() => {
+Pokemon.prototype.attack = function(character2) {
+   var attInterval = setInterval(() => {
   if (character2.isAlive()==true) {
       character2.hp -= this.strength;
 
@@ -49,7 +49,7 @@ Character.prototype.attack = function(character2) {
     }
     else{
         function myStopFunction() {
-            clearInterval(idVar);
+            clearInterval(attInterval);
           }
           myStopFunction()
     }
@@ -57,15 +57,15 @@ Character.prototype.attack = function(character2) {
 };
 
 // method which increases this character's stats when called
-Character.prototype.levelUp = function() {
+Pokemon.prototype.levelUp = function() {
   this.age += 1;
   this.strength += 5;
   this.hp += 25;
 };
 
 // creates two unique characters using the "character" constructor
-var squirttle = new Character("Squirttle", "Water", "Male", 5, 10, 75);
-var pikachu = new Character("Pikachu", "Lightning", "Female", 2, 20, 50);
+var squirttle = new Pokemon("Squirttle", "Water", "Male", 5, 10, 75);
+var pikachu = new Pokemon("Pikachu", "Lightning", "Female", 2, 20, 50);
 
 //   warrior.printStats();
 //   rogue.printStats();
