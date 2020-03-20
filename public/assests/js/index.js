68e3999bd7680cd390d5b68ffe05990b28665aa8
jackson Croy <Jrcroy0607@gmail.com>
2020-03-20 12:22:14 -0700

    added pokemon search and append

--- public/assests/js/index.js
+++ public/assests/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $("#search-btn").on("click", getPokemon)
+    $("#search-btn").on("click", getUserPokemon)
 
-    function getPokemon(e) {
+    function getUserPokemon(e) {
         e.preventDefault();
         let pokeId = $("#search-value").val()
 
@@ -10,6 +10,31 @@ $(document).ready(function() {
             url: "/fight/" + pokeId
         }).then(function(data) {
             console.log(data)
+            $("#userPoke").append(
+                $("<li>").text("Name: " + data.Name),
+                $("<li>").text("HP: " + data.HP),
+                $("<li>").text("Attack: " + data.Attack),
+                $("<li>").text("Defense: " + data.Defense)
+            )
+        })
+    }
+    $("#search-btn2").on("click", getEnemyPokemon)
+
+    function getEnemyPokemon(e) {
+        e.preventDefault();
+        let pokeId = $("#search-value2").val()
+
+        $.ajax({
+            method: "GET",
+            url: "/fight/" + pokeId
+        }).then(function(data) {
+            console.log(data)
+            $("#enemyPoke").append(
+                $("<li>").text("Name: " + data.Name),
+                $("<li>").text("HP: " + data.HP),
+                $("<li>").text("Attack: " + data.Attack),
+                $("<li>").text("Defense: " + data.Defense)
+            )
         })
     }
 });
